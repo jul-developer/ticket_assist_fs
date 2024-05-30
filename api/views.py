52d3162734135557628640b0654849cfaff5e67c
jul <iyulianchesnokov@gmail.com>
2024-05-30 23:03:30 +0300

    authorization and auth system is added

--- api/views.py
+++ api/views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import User
 from django.db.models import Count
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
 
 from api.models import Ticket
 from api.serializers import TicketSerializer
@@ -9,15 +10,21 @@ from api.serializers import TicketSerializer
 class TicketViewSet(viewsets.ModelViewSet):
     queryset = Ticket.objects.all()
     serializer_class = TicketSerializer
+    permission_classes = (IsAuthenticated,)
 
-    # def perform_create(self, serializer):
-    #     serializer.save(creator=self.request.user, worker=get_next_worker())
+    def perform_create(self, serializer):
+        serializer.save(creator=self.request.user, worker=get_next_worker())
 
-    # def get_queryset(self):
-    #     return Ticket.objects.filter(creator=self.request.user)
+    def get_queryset(self):
+        if self.request.user.is_superuser:
+            return Ticket.objects.all()
+        elif self.request.user.is_staff:
+            return Ticket.objects.filter(worker=self.request.user)
+        else:
+            return Ticket.objects.filter(creator=self.request.user)
 
 
-# def get_next_worker():
-#     workers = User.objects.filter(is_staff=True, is_superuser=False).annotate(num_requests=Count('worker'))
-#     workers = sorted(workers, key=lambda worker: worker.num_requests)
-#     return workers[0] if workers else None
+def get_next_worker():
+    workers = User.objects.filter(is_staff=True, is_superuser=False).annotate(num_requests=Count('worker'))
+    workers = sorted(workers, key=lambda worker: worker.num_requests)
+    return workers[0] if workers else None
--- ta_fs/settings.py
+++ ta_fs/settings.py
@@ -106,9 +106,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Moscow'
 
 USE_I18N = True
 
@@ -124,13 +124,13 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# REST_FRAMEWORK = {
-#     'DEFAULT_AUTHENTICATION_CLASSES': (
-#         'rest_framework.authentication.TokenAuthentication',
-#         'rest_framework.authentication.SessionAuthentication',
-#         'rest_framework.authentication.BasicAuthentication',
-#     )
-# }
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+    )
+}
 
 CORS_ORIGIN_WHITELIST = [
     'http://localhost:3000',
