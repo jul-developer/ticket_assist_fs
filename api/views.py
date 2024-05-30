fb4135c112c068adfb29dab89a605a2dc47abd89
jul <iyulianchesnokov@gmail.com>
2024-05-30 12:35:09 +0300

    crud without auth

--- api/views.py
+++ api/views.py
@@ -10,14 +10,14 @@ class TicketViewSet(viewsets.ModelViewSet):
     queryset = Ticket.objects.all()
     serializer_class = TicketSerializer
 
-    def perform_create(self, serializer):
-        serializer.save(creator=self.request.user, worker=get_next_worker())
+    # def perform_create(self, serializer):
+    #     serializer.save(creator=self.request.user, worker=get_next_worker())
 
-    def get_queryset(self):
-        return Ticket.objects.filter(creator=self.request.user)
+    # def get_queryset(self):
+    #     return Ticket.objects.filter(creator=self.request.user)
 
 
-def get_next_worker():
-    workers = User.objects.filter(is_staff=True, is_superuser=False).annotate(num_requests=Count('worker'))
-    workers = sorted(workers, key=lambda worker: worker.num_requests)
-    return workers[0] if workers else None
+# def get_next_worker():
+#     workers = User.objects.filter(is_staff=True, is_superuser=False).annotate(num_requests=Count('worker'))
+#     workers = sorted(workers, key=lambda worker: worker.num_requests)
+#     return workers[0] if workers else None
